bfeb8b9114bebe8deb34847ab1ebeddb44a234e9
kanzd <kanu0704@gmail.com>
2021-06-11 10:15:44 +0530

    11-06-2021

--- payout/urls.py
+++ payout/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/",include("apis.urls"))
+    path("",views.val,name="root"),
+    path("api/",include("apis.urls"),name="apis")
 ]
---
+++ payout/views.py
@@ -0,0 +1,4 @@
+from django import http
+from django.shortcuts import redirect
+def val(req):
+    return redirect("api/")
