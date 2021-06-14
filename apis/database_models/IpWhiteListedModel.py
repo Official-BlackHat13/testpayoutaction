5f17d087d678a53b1cf76ebd7a5b0281e9986fd0
kanzd <kanu0704@gmail.com>
2021-06-14 16:05:21 +0530

    ip whitelisting modifications

--- apis/database_models/IpWhiteListedModel.py
+++ apis/database_models/IpWhiteListedModel.py
@@ -2,8 +2,7 @@ from django.db import models
 
 class IpWhiteListedModel(models.Model):
     id=models.AutoField
-    client=models.IntegerField()
-    client_code=models.CharField(max_length=300)
+    client_model=models.IntegerField()
     ip_address=models.CharField(max_length=300)
     status=models.BooleanField()
     def __str__(self):
