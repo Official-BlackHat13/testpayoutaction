a42218c9791a008c81e16d77fcd71a0d90e40c8e
kanzd <kanu0704@gmail.com>
2021-08-01 12:01:10 +0530

    01-08-2021

--- apis/database_models/DailyLedgerModel.py
+++ apis/database_models/DailyLedgerModel.py
@@ -13,8 +13,8 @@ class DailyLedgerModel(models.Model):
     updated_at=models.DateTimeField(null=True)
     deleted_at=models.DateTimeField(null=True)
     created_by=models.CharField(max_length=3000)
-    updated_by=models.CharField(max_length=3000)
-    deleted_by=models.CharField(max_length=3000)
+    updated_by=models.CharField(max_length=3000,null=True)
+    deleted_by=models.CharField(max_length=3000,null=True)
     def __str__(self):
         return str(self.id)
     
