ea871b6d83556cb6818e74c9bf13ce4d3911a1aa
kanzd <kanu0704@gmail.com>
2021-08-03 01:43:41 +0530

    03-08-2021 phase1-final

--- apis/middlewares.py
+++ apis/middlewares.py
@@ -158,7 +158,7 @@ def MultiTabsRestriction(get_response):
 
 def checkClientStatus(get_response):
     def middleware(req):
-        if req.path!="/api/" and req.path!="/" and "/admin/" not in req.path and req.path!="/api/signup/" and req.path not in "/api/token/" and req.path!="/api/loginrequest/" and req.path!="/api/loginverified/" and req.path!="/api/resendotp/" and req.path!="/api/adminLogin/" and req.path!="/api/adminSignup/" and req.path!="/api/addSlab/" and "/api/getLedgers/" not in req.path and "/api/getTransactionHistory/" not in req.path and req.path!="/api/addBalance/" and "/api/getAllMerchants/" not in req.path and "/api/getLogs/" not in req.path and req.path!="/api/todayInfo/":
+        if req.path!="/api/" and req.path!="/api/addCharge/" and req.path!="/" and "/admin/" not in req.path and req.path!="/api/signup/" and req.path not in "/api/token/" and req.path!="/api/loginrequest/" and req.path!="/api/loginverified/" and req.path!="/api/resendotp/" and req.path!="/api/adminLogin/" and req.path!="/api/adminSignup/" and req.path!="/api/addSlab/" and "/api/getLedgers/" not in req.path and "/api/getTransactionHistory/" not in req.path and req.path!="/api/addBalance/" and "/api/getAllMerchants/" not in req.path and "/api/getLogs/" not in req.path and req.path!="/api/todayInfo/":
             try:
                 merchant_id = req.headers["auth_token"]
                 merchant_id=auth.AESCipher(const.AuthKey,const.AuthIV).decrypt(merchant_id)
