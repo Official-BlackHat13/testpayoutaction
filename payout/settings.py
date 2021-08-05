590c0b49c8b1c9442090282c921dc9210bec46de
kanzd <kanu0704@gmail.com>
2021-08-05 20:51:44 +0530

    05-08-2021 phase1-final

--- payout/settings.py
+++ payout/settings.py
@@ -132,15 +132,15 @@ DATABASES = {
     # }
     # Localdatabase
 
-    #  'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'payout_for_development',
-    #     'USER': 'payout_for_development',
-    #     'PASSWORD': 'payout@123',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    #     # 'socket': '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
-    # }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'payout_for_development',
+        'USER': 'payout_for_development',
+        'PASSWORD': 'payout@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        # 'socket': '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
+    }
     #deployment db
     #  'default': {
     #     'ENGINE': 'django.db.backends.mysql',
@@ -151,15 +151,15 @@ DATABASES = {
     #     'PORT': '3306',
     #     # 'socket': '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
     # }
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stagingPayout',
-        'USER': 'stagingPayout',
-        'PASSWORD': 'ooTee0ie',
-        'HOST': '172.16.157.5',
-        'PORT': '3306',
-        # 'socket': '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
-    }
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'stagingPayout',
+    #     'USER': 'stagingPayout',
+    #     'PASSWORD': 'ooTee0ie',
+    #     'HOST': '172.16.157.5',
+    #     'PORT': '3306',
+    #     # 'socket': '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
+    # }
     # kanishk local db
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
